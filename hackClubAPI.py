9c260639309f5ae4ba94f1afc18c579340ec4d80
James Click <me@dageek.tech>
2020-03-22 14:06:58 -0700

    Cleaning up code, formatting

--- hackClubAPI.py
+++ hackClubAPI.py
@@ -1,24 +1,30 @@
 import requests, json
 
-
+# Custom exception used when HC API returns non-200
 class APIException(Exception):
     pass
 
-
+# Primary API Class
 class HackClubAPI:
 
-    # Remove default in prod
+    # Define API route
     def __init__(self, apiRoot):
         self.apiRoot = apiRoot
 
     def makeRequest(self, username, email):
+        # Dictionary assembly for submission
         userDataDict = {"username": username, "email": email}
+
+        # Making the request
         r = requests.post(self.apiRoot, data=userDataDict)
+
+        # Attempting to interpret the JSON
         try:
             rJson = r.json()
         except:
             raise ValueError("Response was not JSON")
 
+        # Very easy way to return sucess from the function
         if rJson["status"] == "success":
             return r
         else:
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from hackClubAPI import HackClubAPI
+from hackClubAPI import HackClubAPI, APIException
 from github import Github
 from os import environ
 from flask import Flask, jsonify, request
@@ -11,42 +11,57 @@ hcAPI = HackClubAPI(environ["HCURL"])
 mattermostSession = Mattermost("chat.srnd.org", environ["MMKEY"])
 
 
-def submitApplication(username, email):
-    try:
-        username = ghAPI.get_repo("srnd/Pack").get_issue(number=int(id)).user.login
-        email = ghAPI.get_repo("srnd/Pack").get_issue(number=int(id)).body
-        application = hcAPI.makeRequest(username=username, email=email)
-        return True
-    except:
-        return False
-
-
+# Root route, used by Stackdriver ping
 @app.route("/")
 def index():
     return jsonify({"status": 200})
 
 
+# Webhook route used by GitHub
 @app.route("/submit", methods=["POST"])
 def submit():
+
+    # Verify that request is formated correctly
     if request.method == "POST" and request.is_json == True:
         data = request.json
+
+        # Checks POST data if action is applying a label and is the 'Approved' label
         if (
             data["action"] == "labeled"
             and data["issue"]["labels"][0]["name"] == "Approved"
         ):
-            print("Capture")
+            # Used for Stackdriver monitoring, as a fallback
+            print(f"Capture {data['issue']['user']['login']} {data['issue']['body']}")
+
+            # Attempting to make call to Hack Club
             try:
                 application = hcAPI.makeRequest(
                     username=data["issue"]["user"]["login"], email=data["issue"]["body"]
                 )
-            except:
+            except APIException:
+
+                # Send POST data to mattermost in case of error
                 mattermostSession.postToChannel(
                     "codecup-notifs", f"Submission failure ```json{data}```"
                 )
+
+                # JSON data isn't used for anything on the GH side, could be useful in the future
                 return jsonify({"action": "none", "error": "failed hcAPI"})
+
+            except ValueError as error:
+
+                # Send POST data to mattermost in case of error
+                mattermostSession.postToChannel(
+                    "codecup-notifs", f"Interpetation error: {error} ```json{data}```"
+                )
+
+                # JSON data isn't used for anything on the GH side, could be useful in the future
+                return jsonify({"action": "none", "error": "failed JSON Parsing"})
 
             return jsonify({"action": "Submitted"})
         else:
+
+            # Sent when any other action is sent or wrong label
             return jsonify({"action": "none"})
 
 
